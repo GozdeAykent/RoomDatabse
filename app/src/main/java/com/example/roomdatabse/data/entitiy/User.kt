1c4eeaa9a8a281ae62700afa8eb1ee47cdac46f9
gözde <aykentgozde96@gmail.com>
2023-08-23 14:32:55 +0300

    added entitiy

---
+++ app/src/main/java/com/example/roomdatabse/data/entitiy/User.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabse.data.entitiy
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val id:Int =0,
+    val name:String?,
+    val surname:String?,
+    val email:String?,
+    val password:String?
+)
